c909abab7596beb37b267e723932ab4da9b540c7
frou01 <frou01stg44hurutaka@gmail.com>
2025-10-08 16:13:28 +0900

    Add more misc script

---
+++ Packages/com.frou01.utils/Runtime/Udons/AutoDisableSoundPlayer.cs
@@ -0,0 +1,37 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class AutoDisableSoundPlayer : UdonSharpBehaviour
+{
+    [SerializeField] AudioSource targetAudioSource;
+    [SerializeField] float AutoDisableTime;
+    float cnt;
+    void Start()
+    {
+        
+    }
+
+    public void Update()
+    {
+        cnt -= Time.deltaTime;
+        if (cnt < 0)
+        {
+            this.enabled = false;
+            targetAudioSource.enabled = false;
+        }
+    }
+    public void PlayOnshot(AudioClip clip)
+    {
+        this.enabled = true;
+        targetAudioSource.enabled = true;
+        targetAudioSource.PlayOneShot(clip == null ? targetAudioSource.clip : clip);
+        cnt = AutoDisableTime;
+    }
+    public void PlayOnshot()
+    {
+        PlayOnshot(null);
+    }
+}
---
+++ Packages/com.frou01.utils/Runtime/Udons/AutoRepeatSoundPlayer.cs
@@ -0,0 +1,24 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+public class AutoRepeatSoundPlayer : UdonSharpBehaviour
+{
+    //リピート保証付きループ音源
+    [SerializeField] AudioSource targetAudioSource;
+
+    private void OnEnable()
+    {
+        targetAudioSource.Play();
+        SendCustomEventDelayedSeconds(nameof(CheckPlaying), 1);
+    }
+
+    public void CheckPlaying()
+    {
+        if (targetAudioSource.enabled == true && !targetAudioSource.isPlaying)
+        {
+            targetAudioSource.Play();
+        }
+        if (gameObject.activeInHierarchy) SendCustomEventDelayedSeconds(nameof(CheckPlaying), 1);
+    }
+}
