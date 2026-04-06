4b982ba0ec4b3211cf795ceca8cf6ac60f57b878
frou01 <frou01stg44hurutaka@gmail.com>
2026-04-06 18:59:03 +0900

    moved utils from GAC

---
+++ Packages/com.frou01.utils/Runtime/Udons/OptimizationUtility/AnimatorSleeper.cs
@@ -0,0 +1,41 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace frou01.GrabController
+{
+    public class AnimatorSleeper : UdonSharpBehaviour
+    {
+        int isActiveHash;
+
+        int delayer;
+
+        [SerializeField] Animator animator;
+
+        void Start()
+        {
+            isActiveHash = Animator.StringToHash("isActive");
+        }
+        public void Update()
+        {
+            //Debug.Log("debug animator enabled" + animator.enabled);
+            if (animator.GetFloat(isActiveHash) == 0 && animator.enabled)
+            {
+                //Debug.Log("debug_disabling");
+                if (delayer > 10)
+                {
+                    animator.enabled = false;
+                    delayer = 0;
+                }
+                delayer++;
+            }
+            else
+            {
+                delayer = 0;
+            }
+            this.enabled = false;
+        }
+    }
+}
