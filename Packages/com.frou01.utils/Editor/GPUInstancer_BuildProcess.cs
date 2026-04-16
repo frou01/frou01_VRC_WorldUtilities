396eb2119853f3940225d82593721d273ffcda14
frou01 <frou01stg44hurutaka@gmail.com>
2026-04-16 17:23:41 +0900

    Add GPUInstancer(MeshFilterSyncer)

---
+++ Packages/com.frou01.utils/Editor/GPUInstancer_BuildProcess.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using VRC.SDKBase.Editor.BuildPipeline;
+
+namespace frou01.util.editor
+{
+    public class GPUInstancer_BuildProcess : IProcessSceneWithReport, IVRCSDKBuildRequestedCallback
+    {
+        public int callbackOrder => 5;
+
+        public List<GPUInstancer> target = new List<GPUInstancer>();
+
+
+        public void OnProcessScene(Scene scene, BuildReport report)
+        {
+            foreach (GameObject obj in scene.GetRootGameObjects())
+            {
+                //    //Debug.Log("OK!");
+                Proceed(obj.transform);
+            }
+            foreach (GPUInstancer obj in target)
+            {
+                obj.ProceedInstancing();
+            }
+        }
+
+        void Proceed(Transform parent)
+        {
+            target.AddRange(parent.gameObject.GetComponentsInChildren<GPUInstancer>(true));
+        }
+
+        public bool OnBuildRequested(VRCSDKRequestedBuildType requestedBuildType)
+        {
+            return true;
+        }
+    }
+}
