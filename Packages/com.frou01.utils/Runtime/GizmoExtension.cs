f6696d8fb7e34145040171ed2667c1178ad8ce72
frou01 <frou01stg44hurutaka@gmail.com>
2026-07-31 20:31:47 +0900

    Gizmo描画用の拡張を追加

---
+++ Packages/com.frou01.utils/Runtime/GizmoExtension.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace frou01.util
+{
+    public class GizmoExtension : MonoBehaviour
+    {
+        public static Vector3 getCenter(Transform transform)
+        {
+            MeshRenderer[] meshRenderers = transform.GetComponentsInChildren<MeshRenderer>();
+            if (meshRenderers.Length > 0)
+            {
+                Vector3 center = Vector3.zero;
+                foreach (MeshRenderer renderer in meshRenderers)
+                {
+                    center += renderer.bounds.center / meshRenderers.Length;
+                }
+                return center;
+            }
+            else
+            {
+                return transform.position;
+            }
+        }
+
+        public static void DrawArrow(Vector3 start, Vector3 end, float size_len,float size_wide)
+        {
+            SceneView sceneView = SceneView.lastActiveSceneView;
+            Gizmos.DrawLine(start, end);
+            float length = (end - start).magnitude;
+            Vector3 dir = (end - start).normalized;
+            Vector3 look = sceneView.rotation * Vector3.forward;
+            Vector3 wide = Vector3.Cross(dir, look).normalized;
+
+            Gizmos.DrawLine(end - dir * length * size_len + wide * length * size_wide, end);
+            Gizmos.DrawLine(end - dir * length * size_len - wide * length * size_wide, end);
+        }
+    }
+}
