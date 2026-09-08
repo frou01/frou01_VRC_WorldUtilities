3861972b87284b9e3339efb8b8a7655043643c71
frou01 <frou01stg44hurutaka@gmail.com>
2026-09-08 18:24:47 +0900

    AnimatorSleeperもPlaceHolderから変換するようにする

--- Packages/com.frou01.utils/Runtime/Udons/OptimizationUtility/AnimatorSleeper.cs
+++ Packages/com.frou01.utils/Runtime/Udons/OptimizationUtility/AnimatorSleeper.cs
@@ -5,41 +5,9 @@ using VRC.Udon;
 
 namespace frou01.util
 {
-    public class AnimatorSleeper : UdonSharpBehaviour
+    public class AnimatorSleeper : MonoBehaviour
     {
-        int isActiveHash;
-
-        float delayer;
-
-        [SerializeField] Animator animator;
-        [SerializeField] float waitTime = 2;
-
-        void Start()
-        {
-            isActiveHash = Animator.StringToHash("isActive");
-        }
-        public void Update()
-        {
-            //Debug.Log("debug animator enabled" + animator.enabled);
-            if (animator.GetFloat(isActiveHash) == 0 && animator.enabled)
-            {
-                //Debug.Log("debug_disabling");
-                if (delayer > waitTime)
-                {
-                    animator.enabled = false;
-                    ResetCount();
-                }
-                delayer+= Time.deltaTime;
-            }
-            else
-            {
-                ResetCount();
-            }
-            enabled = false;
-        }
-        public void ResetCount()
-        {
-            delayer = 0;
-        }
+        [SerializeField] public Animator animator;
+        [SerializeField] public float waitTime = 2;
     }
 }
--- Packages/com.frou01.utils/Runtime/Udons/OptimizationUtility/AnimatorSleeper_UdonBehaviour.cs
+++ Packages/com.frou01.utils/Runtime/Udons/OptimizationUtility/AnimatorSleeper_UdonBehaviour.cs
@@ -4,10 +4,43 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
-public class AnimatorSleeper_UdonBehaviour : UdonSharpBehaviour
+namespace frou01.util
 {
-    void Start()
+    public class AnimatorSleeper_UdonBehaviour : UdonSharpBehaviour
     {
-        
+        int isActiveHash;
+
+        float delayer;
+
+        [SerializeField] public Animator animator;
+        [SerializeField] public float waitTime = 2;
+
+        void Start()
+        {
+            isActiveHash = Animator.StringToHash("isActive");
+        }
+        public void Update()
+        {
+            //Debug.Log("debug animator enabled" + animator.enabled);
+            if (animator.GetFloat(isActiveHash) == 0 && animator.enabled)
+            {
+                //Debug.Log("debug_disabling");
+                if (delayer > waitTime)
+                {
+                    animator.enabled = false;
+                    ResetCount();
+                }
+                delayer += Time.deltaTime;
+            }
+            else
+            {
+                ResetCount();
+            }
+            enabled = false;
+        }
+        public void ResetCount()
+        {
+            delayer = 0;
+        }
     }
 }
